3db327d55fed5b4bcab29d2423b22dcb1d91576c
HanLubes <mmitofficial@gmail.com>
2019-12-26 18:49:56 +0400

    Update

--- MS SQL To SQLite Converter/MainWindow.xaml.cs
+++ MS SQL To SQLite Converter/MainWindow.xaml.cs
@@ -38,6 +38,7 @@ namespace MS_SQL_To_SQLite_Converter {
             } catch (Exception) {
                 updateStatus("SQLite db test connection failed.");
                 convert_now.IsEnabled = false;
+                convert_now_bw.IsEnabled = false;
             }
         }
 
